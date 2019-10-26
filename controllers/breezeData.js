b35ed3b0ef474adefe4d195d5d831bd3557b2fea
jaebooker <jaesonbooker@gmail.com>
2019-10-26 13:06:35 -0700

    errors fixed, refactoring

--- controllers/breezeData.js
+++ controllers/breezeData.js
@@ -1,29 +1,30 @@
 const logger = require('../services/logger');
 const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
-const { convertFireAlertToRawData, convertAirQualityToRawData } = require('../services/breezeOfMeterAPI');
+const { convertFireAlertToRawData } = require('../services/breezeOfMeterAPI');
 const { respondWith } = require('../services/clientResponse');
 const { asyncHandler } = require('../services/asyncRouterHandler');
 
 module.exports = function(app) {
-    app.use(methodOverride('_method'));
-    app.use(bodyParser.urlencoded({ extended: true }));
+    // app.use(methodOverride('_method'));
+    // app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.get('/airquality', asyncHandler(async (req, res) => {
-      const data = await convertFireAlertToRawData();
-      if (!data) {
-        logger.error(data);
-        return respondWith(res, 500, ['An error occured while getting data.']);
-      }
-      return respondWith(res, 200, 'Data retrieved successfully.', data);
-    }));
+    // app.get('/airquality', asyncHandler(async (req, res) => {
+    //   const data = await convertFireAlertToRawData();
+    //   if (!data) {
+    //     // logger.error(data);
+    //     console.log(data)
+    //     return respondWith(res, 500, ['An error occured while getting data.']);
+    //   }
+    //   return respondWith(res, 200, { data });
+    // }));
 
     app.get('/firealert', asyncHandler(async (req, res) => {
-      const data = await convertAirQualityToRawData();
-      if (!data) {
+      const data = await convertFireAlertToRawData();
+      if (data == undefined) {
         logger.error(data);
         return respondWith(res, 500, ['An error occured while getting data.']);
       }
-      return respondWith(res, 200, 'Data retrieved successfully.', data);
+      return respondWith(res, 200, { data });
     }));
 };
--- services/breezeOfMeterAPI.js
+++ services/breezeOfMeterAPI.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const axios = require('axios');
 const logger = require('./logger');
 //features for air quality to use
@@ -7,41 +8,50 @@ let long = "-122.272743"; //use these for Berkeley
 // let lat = "38.521120" //will later pass in latitue and longitude gotten from user location
 // let long = "-122.812060" //use these for getting a current fire
 //get current fire alerts
-const getBreezeData = axios.create({
-    baseURL: 'https://api.breezometer.com/',
-    headers: { lat: lat, lon: long, key: process.env.breezeKey },
-})
+
 //get current air quality conditions
-const getAirQuality = axios.create({
-    baseURL: 'https://api.breezometer.com/',
-    headers: { lat: lat, lon: long, key: process.env.breezeKey},
-})
-//convert the data
-const convertFireAlertToRawData = async (url) => {
+// const getAirQuality = axios.create({
+//     baseURL: 'https://api.breezometer.com',
+//     headers: { lat: lat, lon: long, key: process.env.breezeKey},
+// })
+// axios.get('https://api.breezometer.com/fires/v1/current-conditions?lat=38.521120&lon=-122.812060&key=3e8fb9e213d949d8b66eba36dffb7e2e', {
+
+const BreezeURI = 'https://api.breezometer.com'
+
+// convert the data
+const convertFireAlertToRawData = async () => {
   logger.info('Coverting data from api.');
-  try {
-    const response = await getBreezeData.get('/fires/v1/current-conditions', {
-      url,
-    });
-    return response.data;
+    try {
+        const response = await axios.get(`${BreezeURI}/fires/v1/current-conditions?lat=${lat}&lon=${long}&key=${process.env.APIKEY}`, {
+          headers: { 'features': features },
+          refresh: false,
+          incognito: false,
+          ipAddress: '32.4.2.223',
+          language: 'en',
+        });
+
+        return response.data;
   } catch (error) {
-    logger.error(error);
+      console.log(error)
+    // logger.error(error);
     return undefined;
   }
 };
 
-const convertAirQualityToRawData = async (url) => {
-  logger.info('Coverting data from api.');
-  try {
-    const response = await getBreezeData.get('/air-quality/v2/current-conditions', {
-      url,
-      headers: { 'features': features },
-    });
-    return response.data;
-  } catch (error) {
-    logger.error(error);
-    return undefined;
-  }
-};
 
-module.exports = { convertFireAlertToRawData, convertAirQualityToRawData };
+
+// const convertAirQualityToRawData = async (url) => {
+//   logger.info('Coverting data from api.');
+//   try {
+//     const response = await getBreezeData.get('/air-quality/v2/current-conditions', {
+//       url,
+//       headers: { 'features': features },
+//     });
+//     return response.data;
+//   } catch (error) {
+//     logger.error(error);
+//     return undefined;
+//   }
+// };
+
+module.exports = { convertFireAlertToRawData };
