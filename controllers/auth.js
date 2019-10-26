8bae4595a71f6dc43f0331dfc5d75f66701ec88b
jaebooker <jaesonbooker@gmail.com>
2019-10-26 16:39:19 -0700

    auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,9 +2,6 @@ const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 
 module.exports = (app) => {
-    app.get('/sign-up', (req, res) => {
-        res.render('sign-up');
-    });
     app.post('/sign-up', (req, res) => {
         const user = new User(req.body);
 
